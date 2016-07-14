8bbd1c0904f7004e469d3cd434509b35356f6b23
chimame <rito.tamata@gmail.com>
2016-07-14 09:36:50 +0900

    Modify Referring to the JavaScript that is output from Webpack

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def assets_path(path)
+    return "http://localhost:4000/#{path}" if Rails.env.development?
+    manifest = Rails.application.config.assets_manifest
+    path = manifest[path] if manifest && manifest[path].present?
+    "/dist/#{path}"
+  end
 end
