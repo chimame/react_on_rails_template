acc40be90b890fafbcac64d480a291a58cb5f14f
chimame <rito.tamata@gmail.com>
2016-10-05 21:13:14 +0900

    Add test for getStyle function of Todo

---
+++ frontend/__tests__/models/todo.test.js
@@ -0,0 +1,15 @@
+import Todo from '../../javascripts/models/todo'
+
+describe('Todo', () => {
+  describe('getStyle()', () => {
+    it('Styles of textDecoration is line-through when completed', () => {
+      const todo = new Todo({completed: true})
+      expect(todo.getStyle().textDecoration).toBe('line-through')
+    })
+
+    it('Styles of textDecoration is none when not completed', () => {
+      const todo = new Todo({completed: false})
+      expect(todo.getStyle().textDecoration).toBe('none')
+    })
+  })
+})
