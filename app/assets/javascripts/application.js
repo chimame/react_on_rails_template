110cafd93ed673564669f170c06ec7dd364560d4
chimame <rito.tamata@gmail.com>
2017-01-31 14:49:42 +0900

    rails generate react_on_rails:install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
+//= require webpack-bundle
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -13,5 +15,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require react_ujs
 //= require_tree .
